cf45ea63b1fcce2dfc06c795379d79170e6ca76b
yamadapc <tacla.yamada@gmail.com>
2020-06-25 08:41:02 +1000

    Get callbacks kind of working

--- src/webkit/mod.rs
+++ src/webkit/mod.rs
@@ -1,5 +1,6 @@
 //! Exposes bindings to the `WKWebView` API.
 pub mod wk_navigation;
+pub mod wk_script_message_handler;
 pub mod wk_web_view;
 pub mod wk_web_view_configuration;
 
--- src/webkit/wk_web_view_configuration.rs
+++ src/webkit/wk_web_view_configuration.rs
@@ -2,6 +2,30 @@ use cocoa::base::id;
 
 #[link(name = "WebKit", kind = "framework")]
 extern "C" {
+    pub static WKUserContentController: id;
+}
+
+pub trait WKUserContentController: Sized {
+    unsafe fn alloc(_: Self) -> id {
+        msg_send![class!(WKWebViewConfiguration), alloc]
+    }
+    unsafe fn init(self) -> id;
+
+    unsafe fn addScriptMessageHandler(self, message_handler: id, name: id) -> id;
+}
+
+impl WKUserContentController for id {
+    unsafe fn init(self) -> id {
+        msg_send![self, init]
+    }
+
+    unsafe fn addScriptMessageHandler(self, message_handler: id, name: id) -> id {
+        msg_send![self, addScriptMessageHandler: message_handler name: name]
+    }
+}
+
+#[link(name = "WebKit", kind = "framework")]
+extern "C" {
     pub static WKWebViewConfiguration: id;
 }
 
@@ -9,12 +33,17 @@ pub trait WKWebViewConfiguration: Sized {
     unsafe fn alloc(_: Self) -> id {
         msg_send![class!(WKWebViewConfiguration), alloc]
     }
-
     unsafe fn init(self) -> id;
+
+    unsafe fn setUserContentController(self, controller: id);
 }
 
 impl WKWebViewConfiguration for id {
     unsafe fn init(self) -> id {
         msg_send![self, init]
+    }
+
+    unsafe fn setUserContentController(self, controller: id) {
+        msg_send![self, setUserContentController: controller]
     }
 }
