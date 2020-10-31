1e8cda92dbedfbdc98b5e9e73de8e689e9a3b939
yamadapc <tacla.yamada@gmail.com>
2020-10-31 14:31:25 +1100

    Add coverage script

---
+++ scripts/build-and-test-with-coverage.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
+
+rustup install nightly
+cargo +nightly build
+cargo +nightly test
+grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
+
