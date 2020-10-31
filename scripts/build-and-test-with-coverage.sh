76d93cbc2ee903cb80dcbc3f3fbd500a689ce4c3
yamadapc <tacla.yamada@gmail.com>
2020-10-31 14:42:42 +1100

    Try fixing workflows

--- scripts/build-and-test-with-coverage.sh
+++ scripts/build-and-test-with-coverage.sh
@@ -7,7 +7,6 @@ export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 export RUSTDOCFLAGS="-Cpanic=abort"
 
-rustup install nightly
 cargo +nightly build
 cargo +nightly test
 grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
