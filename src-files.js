2462d0110d6d404476cd747459dbd2fa5be8eb44
yamadapc <tacla.yamada@gmail.com>
2023-11-15 11:47:42 +0000

    Deploying to gh-pages from  @ 4bbbee3d005b37ea33c46dbccfd38ee6691930de 🚀

--- src-files.js
+++ src-files.js
@@ -17,7 +17,7 @@ var srcIndex = JSON.parse('{\
 "objc":["",[["message",[["apple",[],["mod.rs","x86_64.rs"]]],["mod.rs","verify.rs"]],["rc",[],["autorelease.rs","mod.rs","strong.rs","weak.rs"]]],["declare.rs","encode.rs","lib.rs","macros.rs","runtime.rs"]],\
 "proc_macro2":["",[],["detection.rs","extra.rs","fallback.rs","lib.rs","marker.rs","parse.rs","rcvec.rs","wrapper.rs"]],\
 "quote":["",[],["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]],\
-"syn":["",[["gen",[],["clone.rs","visit.rs"]]],["attr.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","drops.rs","error.rs","export.rs","expr.rs","ext.rs","file.rs","gen_helper.rs","generics.rs","group.rs","ident.rs","item.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","meta.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","pat.rs","path.rs","print.rs","punctuated.rs","restriction.rs","sealed.rs","span.rs","spanned.rs","stmt.rs","thread.rs","token.rs","ty.rs","verbatim.rs","whitespace.rs"]],\
+"syn":["",[["gen",[],["clone.rs"]]],["attr.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","drops.rs","error.rs","export.rs","expr.rs","ext.rs","file.rs","gen_helper.rs","generics.rs","group.rs","ident.rs","item.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","meta.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","pat.rs","path.rs","print.rs","punctuated.rs","restriction.rs","sealed.rs","span.rs","spanned.rs","stmt.rs","thread.rs","token.rs","ty.rs","verbatim.rs","whitespace.rs"]],\
 "unicode_ident":["",[],["lib.rs","tables.rs"]]\
 }');
 createSrcSidebar();
