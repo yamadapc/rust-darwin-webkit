22f1ab4e3b043775f86750033ed842b18ae2dd61
yamadapc <tacla.yamada@gmail.com>
2023-11-15 11:19:28 +0000

    Update docs with examples

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,13 @@
 //!
 //! It uses the `objc` and `cocoa` crates to bind with Objective-C.
 //!
+//! **This has not been tested properly yet.**
+//!
+//! Can be embedded onto audio plug-ins by getting the native webview handle and
+//! adding it to a plug-ins native `NSWindowView` handle.
+//!
+//! **Closure captures are unsafe as we'll just pass pointers around.**
+//!
 //! The `darwin_webkit::foundation` module exposes some dependencies to using
 //! the WKWebView APIs, like `NSURLRequest`.
 //!
@@ -23,6 +30,78 @@
 //! * `darwin_webkit::helpers::DarwinWKWebView::evaluate_javascript`
 //! * `darwin_webkit::helpers::DarwinWKWebView::load_url`
 //! * `darwin_webkit::helpers::DarwinWKWebView::load_html_string`
+//!
+//! ## High-level wrapper
+//!
+//! Raw bindings are exposed, but under the `darwin_webkit::helpers` module, some high-level
+//! rust wrappers are provided.
+//!
+//! ### High-level Example
+//!
+//! ```rust
+//! use darwin_webkit::helpers::dwk_app::DarwinWKApp;
+//!
+//! unsafe {
+//!     let app = DarwinWKApp::new("Simple WebView");
+//!     let webview = app.create_webview();
+//!     webview.load_url("https://www.google.com.br");
+//!     app.set_webview(&webview);
+//!     // Then run with:
+//!     // app.run();
+//! }
+//! ```
+//!
+//! ### High-level Communication example
+//! ```rust
+//! use cocoa::base::id;
+//! use darwin_webkit::helpers::dwk_app::DarwinWKApp;
+//! use std::rc::Rc;
+//!
+//! unsafe {
+//!     let app = DarwinWKApp::new("Host an app");
+//!     let webview = Rc::new(app.create_webview());
+//!
+//!     let callback = Box::into_raw(Box::new(Box::new(|_: id, _: id| {
+//!         println!("JavaScript called rust!");
+//!         webview.evaluate_javascript("document.body.innerHTML += ' -> response from rust';");
+//!     })));
+//!     webview.add_message_handler("hello", callback);
+//!     webview.load_html_string(
+//!         "
+//!         <script>
+//!             document.body.innerHTML += 'start';
+//!             window.webkit.messageHandlers.hello.postMessage('hello');
+//!             document.body.innerHTML += ' -> success';
+//!         </script>
+//!         ",
+//!         "",
+//!     );
+//!
+//!     app.set_webview(&webview);
+//!     // Then run with:
+//!     // app.run();
+//! }
+//! ```
+//!
+//! ## Missing
+//!
+//! - ~~Callbacks from JavaScript to Rust.~~
+//! - TODOs in raw bindings
+//! * `serverTrust` in `WKWebView`
+//! * In `NSURLRequest`
+//! * Working with a Cache Policy
+//! * Accessing Request Components
+//! * Getting Header Fields
+//! * Controlling Request Behavior
+//! * Accessing the Service Type
+//! * Supporting Secure Coding
+//! - ...
+//!
+//! ## Linting
+//! ```bash
+//! cargo clippy
+//! ```
+
 #![allow(non_snake_case)]
 
 #[macro_use]
